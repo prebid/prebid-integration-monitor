7b3bd0500d3b50e69d8c242a780c3d57f7083f71
Chris-Prebid <phoenixtechnerd@gmail.com>
2025-02-10 07:55:26 -0700

    update daily results

--- prebid.js
+++ prebid.js
@@ -118,7 +118,7 @@ async function prebidExplorer() {
 
             // Write results as a JSON array
             const jsonOutput = JSON.stringify(results, null, 2);  // Pretty print with 2 spaces
-            fs.appendFileSync('output/Feb/8th.json', jsonOutput, 'utf8');
+            fs.appendFileSync('output/Feb/9th.json', jsonOutput, 'utf8');
             console.log('Results have been saved');
         } catch (err) {
             console.error('Failed to write results:', err);
